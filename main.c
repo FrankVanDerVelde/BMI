039f408980600c9822ccdad8b711f93cb3cdb69c
FrankVanDerVelde <frankvdv1998@gmail.com>
2016-12-02 00:00:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -16,19 +16,19 @@ int main()
 
      printf( "Uw bmi is %f \n", bmi );
 
-     if (bmi < 18.5) {
+     if (bmi <= 18.5) {
         printf("Je hebt een mooi doutze kroes firguurtje ");
      }
 
-     if (bmi > 18.5 && bmi <25) {
+     if (bmi => 18.5 && bmi <25) {
         printf("Je hebt een gezond bmi");
      }
 
-     if (bmi > 24.9 && bmi <40) {
+     if (bmi => 24.9 && bmi <40) {
         printf("Je bent te zwaar, net als mauro's voorhoofd.");
      }
 
-     if (bmi > 39) {
+     if (bmi => 39) {
         printf("Je bent net zo dik als nanko's ego (Obese)");
      }
 }
